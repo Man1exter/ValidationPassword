b9aca84f27b60cc16067987de589398b149a4ad7
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-02 21:14:20 +0200

    COMPLEET validation password wit hdesk js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const input = document.querySelector("input");
-const p = document.querySelector("miner");
+const p = document.querySelector(".miner");
 
 
 const letters = /[a-z]/i;
@@ -7,6 +7,23 @@ const number = /[0-9]/;
 const special = /[!@#$%^&*]/;
 const minValue = 10;
 
+
+
+const waitPassword = () => {
+
+if (input.value.length > minValue && input.value.match(letters)&& input.value.match(numbers)&& input.value.match(special)) {
+input.style.color = "green";
+p.style.color = "green";
+
+} else if (input.value.length > minValue && input.value.match(letters)&& input.value.match(numbers)) {
+    input.style.color = "orange";
+    p.style.color = "orange";
+
+} else {
+    input.style.color = "red";
+    p.style.color = "red";
+  }
+};
 
 const check = () => {
     if (input.value !== ''){
@@ -17,4 +34,4 @@ const check = () => {
 };
 
 
-input.addEventListener("keyup", check)
+input.addEventListener("keyup", check);
