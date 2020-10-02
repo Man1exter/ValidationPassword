c1108288c57e11c5b01bab0344c3af3adeff6376
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-02 20:55:52 +0200

    regexp contains with functrions on js

---
+++ main.js
@@ -0,0 +1,10 @@
+const input = document.querySelector("input");
+const p = document.querySelector("miner");
+
+
+const letters = /[a-z]/i;
+const number = /[0-9]/;
+const special = /[!@#$%^&*]/;
+const minValue = 10;
+
+
